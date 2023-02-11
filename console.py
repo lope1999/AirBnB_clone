b4d5429ed65eb410e1a3d72cc46d0cf63fc1dd9d
SULIAT TITILOPE ALAGA <41836834+lope1999@users.noreply.github.com>
2023-02-11 17:26:05 +0100

    Update console.py

--- console.py
+++ console.py
@@ -6,31 +6,20 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
 import json
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     ''' HBNB class contains entry point '''
 
     prompt = '(hbnb) '
-    myclasses = ["BaseModel", "User", "Place", "State", "Amenity", "Review",\
-                 "City"]
 
     def do_EOF(self, line):
         ''' exit the program '''
         return True
 
-    def help_EOF(self):
-        ''' help EOF'''
-        print ("EOF command to exit the program\n")
-
     def help_quit(self):
         ''' help quit '''
-        print ("Quit command to exit the program\n")
+        print("Quit command to exit the program\n")
 
     def do_quit(self, arg):
         ''' quit interpreter '''
@@ -44,13 +33,13 @@ class HBNBCommand(cmd.Cmd):
         ''' create a new instance of '''
         if len(classname) == 0:
             print('** class name missing **')
-        elif classname not in self.myclasses:
-                print('** class doesn\'t exist **')
-                return False
         else:
-            new = eval("{}()".format(classname))
-            new.save()
-            print(new.id)
+            try:
+                new = eval("{}()".format(classname))
+                new.save()
+                print(new.id)
+            except:
+                print('** class doesn\'t exist **')
 
     def help_create(self):
         ''' help create '''
@@ -61,11 +50,12 @@ class HBNBCommand(cmd.Cmd):
         args = line.split()
         if len(args) == 0:
             print('** class name missing **')
-            return False
-        elif args[0] not in self.myclasses:
-            print('** class doesn\'t exist **')
-            return False
-
+        else:
+            try:
+                eval("{}()".format(args[0]))
+            except:
+                print('** class doesn\'t exist **')
+                return False
         if len(args) < 2:
             print('** instance id missing **')
             return False
@@ -74,12 +64,12 @@ class HBNBCommand(cmd.Cmd):
         for i in all_objs.keys():
             if i == "{}.{}".format(args[0], args[1]):
                 print(all_objs[i])
-                return False
+                return
         print('** no instance found **')
 
     def help_show(self):
         ''' help show '''
-        print("Show command to display the string representation of class\n")
+        print("Show command to show the classes\n")
 
     def do_destroy(self, line):
         ''' deletes an instance based on the class id'''
@@ -87,43 +77,50 @@ class HBNBCommand(cmd.Cmd):
         if len(line) == 0:
             print('** class name missing **')
             return False
-        elif args[0] not in self.myclasses:
-            print('** class doesn\'t exist ** ')
-            return False
-        elif len(args) < 2:
+        else:
+            try:
+                eval("{}()".format(args[0]))
+            except:
+                print('** class doesn\'t exist ** ')
+        if len(args) < 2:
             print('** instance id missing **')
             return False
-        else:
-            all_objs = storage.all()
-            for i in all_objs:
-                if i == "{}.{}".format(args[0], args[1]):
-                    all_objs.pop(i)
-                    storage.save()
-                    return False
-            print('** no instance found **')
+
+        all_objs = storage.all()
+        for i in all_objs:
+            if i == "{}.{}".format(args[0], args[1]):
+                all_objs.pop(i)
+                storage.save()
+                return
+        print('** no instance found **')
 
     def help_destroy(self):
         ''' help destroy '''
-        print("Destroy command to destroy an object\n")
+        print("Destroy command to destroy a class\n")
 
     def do_all(self, line):
         ''' prints all string representations of instances'''
         args = line.split()
         all_objs = storage.all()
+        final = []
 
         if len(args) == 0:
             for i in all_objs:
                 strarg = str(all_objs[i])
-                print(strarg)
-        elif line not in self.myclasses:
-            print('** class doesn\'t exist **')
-            return False
-        else:
+                final.append(strarg)
+            if final != []:
+                print(final)
+
+        elif len(args) > 0:
             for i in all_objs:
                 if i.startswith(args[0]):
                     strarg = str(all_objs[i])
-                    print(strarg)
-        return False
+                    final.append(strarg)
+            if final == []:
+                print('** class doesn\'t exist **')
+                return False
+            else:
+                print(final)
 
     def help_all(self):
         ''' help all'''
@@ -170,20 +167,17 @@ class HBNBCommand(cmd.Cmd):
             print('** value missing **')
             return False
 
-        if updatevalue.isdecimal() is True:
-            setattr(clschange, attributename, int(updatevalue))
+        try:
+            setattr(clschange, attributename, float(updatevalue))
             storage.save()
-        else:
-            try:
-                setattr(clschange, attributename, float(updatevalue))
-                storage.save()
-            except:
-                setattr(clschange, attributename, str(updatevalue))
-                storage.save()
+        except:
+            setattr(clschange, attributename, str(updatevalue))
+            storage.save()
 
     def help_update(self):
         '''help update'''
-        print("update command to update attributes\n")
+        print("update command to update attributes")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
