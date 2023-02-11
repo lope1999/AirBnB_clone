51e0e17c3b240e208397d6544b2867f8b08d0fe2
Saheed Rasheed <111002271+Jocatemir@users.noreply.github.com>
2023-02-11 17:32:15 +0100

    Update console.py

--- console.py
+++ console.py
@@ -6,16 +6,27 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
 import json
-
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     ''' HBNB class contains entry point '''
 
     prompt = '(hbnb) '
+    myclasses = ["BaseModel", "User", "Place", "State", "Amenity", "Review",\
+                 "City"]
 
     def do_EOF(self, line):
         ''' exit the program '''
         return True
+
+    def help_EOF(self):
+        ''' help EOF'''
+        print ("EOF command to exit the program\n")
 
     def help_quit(self):
         ''' help quit '''
@@ -33,13 +44,13 @@ class HBNBCommand(cmd.Cmd):
         ''' create a new instance of '''
         if len(classname) == 0:
             print('** class name missing **')
-        else:
-            try:
-                new = eval("{}()".format(classname))
-                new.save()
-                print(new.id)
-            except:
+        elif classname not in self.myclasses:
                 print('** class doesn\'t exist **')
+                return False
+        else:
+            new = eval("{}()".format(classname))
+            new.save()
+            print(new.id)
 
     def help_create(self):
         ''' help create '''
@@ -50,12 +61,11 @@ class HBNBCommand(cmd.Cmd):
         args = line.split()
         if len(args) == 0:
             print('** class name missing **')
-        else:
-            try:
-                eval("{}()".format(args[0]))
-            except:
-                print('** class doesn\'t exist **')
-                return False
+            return False
+        elif args[0] not in self.myclasses:
+            print('** class doesn\'t exist **')
+            return False
+
         if len(args) < 2:
             print('** instance id missing **')
             return False
@@ -64,12 +74,12 @@ class HBNBCommand(cmd.Cmd):
         for i in all_objs.keys():
             if i == "{}.{}".format(args[0], args[1]):
                 print(all_objs[i])
-                return
+                return False
         print('** no instance found **')
 
     def help_show(self):
         ''' help show '''
-        print("Show command to show the classes\n")
+        print("Show command to display the string representation of class\n")
 
     def do_destroy(self, line):
         ''' deletes an instance based on the class id'''
@@ -77,50 +87,43 @@ class HBNBCommand(cmd.Cmd):
         if len(line) == 0:
             print('** class name missing **')
             return False
-        else:
-            try:
-                eval("{}()".format(args[0]))
-            except:
-                print('** class doesn\'t exist ** ')
-        if len(args) < 2:
+        elif args[0] not in self.myclasses:
+            print('** class doesn\'t exist ** ')
+            return False
+        elif len(args) < 2:
             print('** instance id missing **')
             return False
-
-        all_objs = storage.all()
-        for i in all_objs:
-            if i == "{}.{}".format(args[0], args[1]):
-                all_objs.pop(i)
-                storage.save()
-                return
-        print('** no instance found **')
+        else:
+            all_objs = storage.all()
+            for i in all_objs:
+                if i == "{}.{}".format(args[0], args[1]):
+                    all_objs.pop(i)
+                    storage.save()
+                    return False
+            print('** no instance found **')
 
     def help_destroy(self):
         ''' help destroy '''
-        print("Destroy command to destroy a class\n")
+        print("Destroy command to destroy an object\n")
 
     def do_all(self, line):
         ''' prints all string representations of instances'''
         args = line.split()
         all_objs = storage.all()
-        final = []
 
         if len(args) == 0:
             for i in all_objs:
                 strarg = str(all_objs[i])
-                final.append(strarg)
-            if final != []:
-                print(final)
-
-        elif len(args) > 0:
+                print(strarg)
+        elif line not in self.myclasses:
+            print('** class doesn\'t exist **')
+            return False
+        else:
             for i in all_objs:
                 if i.startswith(args[0]):
                     strarg = str(all_objs[i])
-                    final.append(strarg)
-            if final == []:
-                print('** class doesn\'t exist **')
-                return False
-            else:
-                print(final)
+                    print(strarg)
+        return False
 
     def help_all(self):
         ''' help all'''
@@ -167,16 +170,20 @@ class HBNBCommand(cmd.Cmd):
             print('** value missing **')
             return False
 
-        try:
-            setattr(clschange, attributename, float(updatevalue))
+        if updatevalue.isdecimal() is True:
+            setattr(clschange, attributename, int(updatevalue))
             storage.save()
-        except:
-            setattr(clschange, attributename, str(updatevalue))
-            storage.save()
+        else:
+            try:
+                setattr(clschange, attributename, float(updatevalue))
+                storage.save()
+            except:
+                setattr(clschange, attributename, str(updatevalue))
+                storage.save()
 
     def help_update(self):
         '''help update'''
-        print("update command to update attributes")
+        print("update command to update attributes\n")
 
 
 if __name__ == '__main__':
