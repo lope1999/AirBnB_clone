fda95e842fcc4b3066e2ec4123f83da6cc557200
Jocatemir <rasheedsaheed5658@gmail.com>
2023-02-11 17:51:08 +0100

    updating

---
+++ test_module.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+def add(a: int, b: int) -> float:
+    """
+    This function add two integers
+    Return Float
+    """
+    return a+b
+
+
+def sub(a: int, b: int) -> float:
+    """
+    This function subtract two integers
+    Return Float
+    """
+    return a-b
+
+
+def multiply(a: int, b: int) -> float:
+    """
+    This function multiply two integers
+    Return Float
+    """
+    return a*b
+
+
+def div(a: int, b: int) -> float:
+    """
+    This function divide two integers
+    Return Float
+    """
+    return a/b
